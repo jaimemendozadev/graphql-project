2aa2fc939fd1f8ebe5c9487551816867629cacff
jaimemendozadev <jaimemendozadev@gmail.com>
2017-08-30 19:56:35 -0700

    Setup axios get requests and json-server

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/graphql', expressGraphQL({
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, ()=> {
   console.log(`Listening on port ${port}`);
